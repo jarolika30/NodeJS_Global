2eb7f7fe8ad026ab5143aa2b40c9e525bb47cd58
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2021-04-25 20:48:39 +0300

    feat: install postgree, create and insert data into table Users

---
+++ dataUsers.sql
@@ -0,0 +1,13 @@
+INSERT INTO Users(name, login, password, age, isDeleted)
+VALUES
+('Ann Becker', 'ann_becker@gmail.com', 'we567fgh', 23, false),
+('John Run', 'joh_run@gmail.com', '890fdghkuyyyy', 64, false),
+('Nil Shallow', 'nil_shallow@gmail.com', '09etyadmlfk', 34, false),
+('Olga Orlik', 'olga_orlik@gmail.com', 'lkder73as', 18, false),
+('Tom Sadfry', 'tom_sadfry@gmail.com', 'kie3421qws', 29, false),
+('Lili Gurt', 'lili_gurt@gmail.com', '89ty654dfgvc', 45, false),
+('Sam Kastar', 'sam_kastar@gmail.com', '8rtydlmcbvbvt1', 47, false),
+('Ann Walsy', 'ann_walsy@gmail.com', 'iohdg653rse', 16, false),
+('Rob Foggy', 'rob_foggy@gmail.com', '985rtdewon', 21, false),
+('Lena Smart', 'lena_smart@gmail.com', 'jhgddfsrty', 83, true),
+('Samanta Hall', 'samanta_hall@gmail.com', 'we567fgh', 33, false);
