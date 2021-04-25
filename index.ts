fe48baadea425ceda86efef6d10e5f6479c8aab6
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2021-04-25 13:21:26 +0300

    refactor: remove comments

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { Users } from './src/mockData/userCollection';
 import { PORT } from './src/constants';
 
 const express = require('express');
@@ -13,14 +12,3 @@ app.use(express.json());
 app.use(router);
 
 app.use('/api', router);
-
-/* router.get('/', (req, res) => {
-  res.header("Content-Type",'application/json');
-  res.send(JSON.stringify(Users, null, 4));
-})
-
-router.post('/user', (req, res) => {
-  let user = req.body;
-
-  res.status(204).send();
-}) */
