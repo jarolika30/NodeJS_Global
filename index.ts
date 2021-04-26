34cde78fe939aab64cc5927b23c327a7837a3946
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2021-04-26 21:00:13 +0300

    feat: make connection to postgre db

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { PORT } from './src/constants';
+import { db } from './src/config/postgre.config';
 
 const express = require('express');
 const router = require('./src/server/userRoutes');
 const app = express();
+db.authenticate()
+.then(()=> console.log("DB Connected..."))
+.catch(err => console.log("ERROR" + err))
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}!`);
--- src/config/postgree.config.ts
+++
@@ -1,5 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("nodejsrest", "postgres", "nextPass21", {
-  dialect: "postgres",
-  host: "localhost"
-});
